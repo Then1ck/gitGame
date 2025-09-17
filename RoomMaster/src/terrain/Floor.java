598b595777b3bf7600cf89cdd07ad74845e37d31
Then1ck <nickymarcellinokwan@gmail.com>
2025-09-17 12:50:37 +0700

    Floor

---
+++ RoomMaster/src/terrain/Floor.java
@@ -0,0 +1,17 @@
+package terrain;
+
+import entities.Entities;
+
+public class Floor {
+	private int left, right, y;
+
+	public Floor(int Left, int Right, int Y) {
+		left = Left;
+		right = Right;
+		y = Y;
+	}
+	
+	public void lands(Entities nt) {
+		
+	}
+}
